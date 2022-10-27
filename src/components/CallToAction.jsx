abaa638ba3698502e9eb22af85b0171fd1288836
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-27 18:58:26 +0100

    CTA Component Done & Reverse GetStartButton class added

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -1,5 +1,17 @@
+import GetStartButton from "./GetStartButton";
+
 function CallToAction() {
-  return <div>CallToAction</div>;
+  return (
+    <section className="bg-brightRed" id="cta">
+      <div className="container flex flex-col items-center justify-between px-6 py-24 mt-12 mx-auto space-y-12 md:py-12 md:flex-row md:space-y-0">
+        <h2 className="text-5xl font-bold leading-tight text-center text-white md:text-4xl md:max-w-xl md:text-left">
+          Simplify how your team works today
+        </h2>
+
+        <GetStartButton reverse={true} />
+      </div>
+    </section>
+  );
 }
 
 export default CallToAction;
--- src/components/GetStartButton.jsx
+++ src/components/GetStartButton.jsx
@@ -1,12 +1,12 @@
-function GetStartButton({ hidden = false }) {
+function GetStartButton({ hidden = false, reverse = false }) {
   return (
     <a
       href=""
-      className={
-        hidden
-          ? "hidden p-3 px-6 text-white bg-brightRed rounded-full hover:bg-brightRedLight md:block "
-          : "p-3 px-6 text-white bg-brightRed rounded-full hover:bg-brightRedLight"
-      }
+      className={`${hidden ? "hidden" : ""} ${
+        reverse
+          ? "bg-white text-brightRed shadow-xl hover:bg-gray-300"
+          : "bg-brightRed text-white hover:bg-brightRedLight"
+      } p-3 px-6 text-white bg-brightRed rounded-full  md:inline-block`}
     >
       Get Started
     </a>
