c6f31cec774150edfa05194263d5a62cad61a635
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-27 18:09:33 +0100

    Testimonials Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import FeatureSection from "./components/FeatureSection";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <FeatureSection />
+      <Testimonials />
     </>
   );
 }
