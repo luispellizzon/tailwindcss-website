d66ea1d8740dc6b7774991d678a3c8a0ecb7f4df
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-27 16:31:26 +0100

    Hero Section Completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section id="hero">
       {/* Flex Container */}
-      <div className="container flex flex-col-reverse md:flex-row justify-center px-6 mx-auto mt-10 space-y-0 md:space-y-0">
+      <div className="container flex flex-col-reverse justify-center px-6 mx-auto mt-10 space-y-0 md:flex-row  md:space-y-0 md:space-x-5">
         {/* Left container */}
         <div className="flex flex-col mb-32 space-y-12">
           <h1 className="max-w-sm text-4xl font-bold text-center md:text-4xl md:text-left">
@@ -20,7 +20,7 @@ const Hero = () => {
           </div>
         </div>
         {/* Right Container with Imgs */}
-        <div className="md:w-[70%] lg:w-1/2">
+        <div className="lg:w-[45%]">
           <img src={IllustrationInto} alt="" />
         </div>
       </div>
