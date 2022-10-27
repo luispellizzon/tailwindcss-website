c71f9fa760e6d3713b33f8fd9651ca43c8b3329a
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-27 18:35:12 +0100

    Testimonials Section Completed

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,13 +1,14 @@
 import Person1 from "../assets/img/avatar-anisha.png";
 import Person2 from "../assets/img/avatar-richard.png";
 import Person3 from "../assets/img/avatar-ali.png";
+import GetStartButton from "./GetStartButton";
 
 function Testimonials() {
   return (
     <section id="testimonials">
       <div className="max-w-6xl px-5 mx-auto mt-32 text-center">
         <h2 className="text-4xl font-bold">What Our Customers Say </h2>
-        <div className="container flex flex-col mx-auto space-y-12 mt-24 md:flex-row md:space-x-6 md:space-y-0">
+        <div className="container flex flex-col mx-auto space-y-12 mt-24 mb-10 md:flex-row md:space-x-6 md:space-y-0">
           {/* Testimonial 1*/}
           <div className="flex flex-col items-center p-6 space-y-6 rounded-lg bg-brightRedSupLight relative md:w-1/3">
             <img src={Person1} alt="" className="absolute w-16 -top-8" />
@@ -20,8 +21,9 @@ function Testimonials() {
               </q>
             </blockquote>
           </div>
+          {/* Add REACT SLIDER LATER */}
           {/* Testimonial 2*/}
-          <div className="flex flex-col items-center p-6 space-y-6 rounded-lg bg-brightRedSupLight relative md:w-1/3">
+          <div className="hidden flex-col items-center p-6 space-y-6 rounded-lg bg-brightRedSupLight relative md:flex md:w-1/3">
             <img src={Person2} alt="" className="absolute w-16 -top-8" />
             <h5 className="text-lg font-bold">Richard Bravo</h5>
             <blockquote className="text-sm text-darkGrayishBlue">
@@ -33,7 +35,7 @@ function Testimonials() {
             </blockquote>
           </div>
           {/* Testimonial 3*/}
-          <div className="flex flex-col items-center p-6 space-y-6 rounded-lg bg-brightRedSupLight relative md:w-1/3">
+          <div className="hidden flex-col items-center p-6 space-y-6 rounded-lg bg-brightRedSupLight relative md:flex md:w-1/3">
             <img src={Person3} alt="" className="absolute w-16 -top-8" />
             <h5 className="text-lg font-bold">Ali Culey</h5>
             <blockquote className="text-sm text-darkGrayishBlue">
@@ -45,6 +47,7 @@ function Testimonials() {
             </blockquote>
           </div>
         </div>
+        <GetStartButton />
       </div>
     </section>
   );
