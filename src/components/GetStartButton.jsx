f5f352649b4fcd95638e5705c02f6687b62e5f03
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-27 18:59:07 +0100

    Changes on Button

--- src/components/GetStartButton.jsx
+++ src/components/GetStartButton.jsx
@@ -6,7 +6,7 @@ function GetStartButton({ hidden = false, reverse = false }) {
         reverse
           ? "bg-white text-brightRed shadow-xl hover:bg-gray-300"
           : "bg-brightRed text-white hover:bg-brightRedLight"
-      } p-3 px-6 text-white bg-brightRed rounded-full  md:inline-block`}
+      } p-3 px-6 rounded-full  md:inline-block`}
     >
       Get Started
     </a>
